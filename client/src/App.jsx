7bf72d3f98021a56cfe7f357a77b5cdc07822667
dungnguyen-kma <dungnguyen.kma@gmail.com>
2025-03-06 10:02:24 +0700

    init base

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -76,9 +76,9 @@ function App() {
         <Route
           path="/shop"
           element={
-            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            // <CheckAuth isAuthenticated={isAuthenticated} user={user}>
               <ShoppingLayout />
-            </CheckAuth>
+            // </CheckAuth>
           }
         >
           <Route path="home" element={<ShoppingHome />} />
--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -1,9 +1,9 @@
 const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
-  mode: "",
-  client_id: "",
-  client_secret: "",
+  mode: "sandbox",
+  client_id: "AXGGkH2ncU-QhvTB_ItwNrrCeLmSYQ3LpW5yuvEV4CB239oQeuRHunwicqRPjmgouh_bEnXI-aOOC0pN",
+  client_secret: "EFzSX7-t944Q22QVdWN3pD1zMbqg-dJJPNPz0bEU9bX9ZGLKh8asBXnVfMn65tu7N8kPKSGFIREvelMX",
 });
 
 module.exports = paypal;
